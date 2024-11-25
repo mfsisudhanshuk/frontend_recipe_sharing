e468fb9795ee86cb7cc87ec8dbac95357c50e30a
mfsi.sudhanshu <mfsi.sudhanshuk>
2024-11-25 23:06:00 +0530

    cypress window resize

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "cypress";
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implement node event listeners here
     },
   },
+  viewportWidth: 1920, 
+  viewportHeight: 1080,
 });
